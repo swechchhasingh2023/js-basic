65401daacff457ab807df6606baa27179db3c6cd
swechchhasingh2023 <168284519+swechchhasingh2023@users.noreply.github.com>
2024-06-18 17:44:51 +0000

    operations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -8,16 +8,16 @@
 let score = "33abc" // type NaN
 
 
-console.log(typeof score);
+// console.log(typeof score);
 // different way to print this using method 
 
-console.log(typeof (score));
+// console.log(typeof (score));
 
 // let supooese we are getting data from another which is not confirmed that is number 
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber); // NaN (special type) => not a number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // NaN (special type) => not a number
 
 // 33 => 33
 // "33abc" => NaN
@@ -25,7 +25,7 @@ console.log(valueInNumber); // NaN (special type) => not a number
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0=> false
 // "" => true
@@ -34,5 +34,42 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//************************************ Operations ***********************/
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
